d59a43bba9cb6135b16cbee9e2e31157ef90f0f3
Shreya612 <57206392+Shreya612@users.noreply.github.com>
2019-10-31 18:08:03 +0530

    Update file.c

--- file.c
+++ file.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-    int a[10][10], b[10][10], result[10][10], r1, c1, r2, c2, i, j;
+    int a[10][10], b[10][10], result[10][10], r1, c1, r2, c2, i, j, k;
 
     printf("Enter rows and column for first matrix: ");
     scanf("%d %d", &r1, &c1);
